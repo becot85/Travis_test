242366948068cbbf23dc617c689fad30627f8bf8
Benoit Cote <benoit.cote.4@ulaval.ca>
2016-03-19 21:09:43 -0400

    BC: all python commands in the test_01.py script

--- travis_test_suites/test_01.py
+++ travis_test_suites/test_01.py
@@ -1,6 +1,12 @@
+# Import the sys package to return exit codes
 import sys
+
 def try_exit(i=0):
     if i == 0:
         sys.exit(0)
     else:
         sys.exit(1)
+
+print 'I got in!!'
+try_exit(i=0)
+
